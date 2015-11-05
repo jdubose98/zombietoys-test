66ab65913bcf6895bcf7c7602228bdeac1214f8b
Jacob Dubose <jdubose@utc4me.org>
2015-11-05 10:28:52 -0500

    Powerup orbs sort of work now
    I mean you can walk into them and they disappear, but they're not
    exactly working correctly in terms of modifying the player
    temporarily...

---
+++ ZT/Assets/Scripts/OrbScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OrbScript : MonoBehaviour {
+
+    [SerializeField] BoostTypes Type = BoostTypes.FireRate;
+    [SerializeField] Text InfoText;
+    [SerializeField] AudioSource PowerupSound;
+    [SerializeField] PlayerShooting ShootScript;
+
+    // Max spawn pos Z is 22.7
+    // Min pos Z is -22.7
+    // Max X is 25, min X -25
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void OnTriggerEnter(Collider otherObject)
+    {
+        PowerupSound.Play();
+        switch(Type){
+            case BoostTypes.DoubleDamage:
+                BulletDamageBoost(80, 5, 40);
+                break;
+            case BoostTypes.FireRate:
+                break;
+            case BoostTypes.Shotgun:
+                break;
+        }
+        
+        DestroyObject(gameObject);
+    }
+
+    enum BoostTypes
+    {
+        FireRate,
+        Shotgun,
+        DoubleDamage
+    }
+
+    IEnumerator BulletDamageBoost(int startValue, float waitTime, int endValue) // Coroutines are cool.
+    {
+        Debug.Log("Player got double damage!");
+        ShootScript.damagePerShot = startValue;
+        yield return new WaitForSeconds(waitTime);
+        ShootScript.damagePerShot = endValue;
+        Debug.Log("Player lost double damage!");
+    }
+}
--- ZT/Assets/Scripts/Player/PlayerHealth.cs
+++ ZT/Assets/Scripts/Player/PlayerHealth.cs
@@ -2,84 +2,114 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class PlayerHealth : MonoBehaviour
-{
-    public int startingHealth = 100;
-    public int currentHealth;
-    public Slider healthSlider;
-    public Image damageImage;
-    public AudioClip deathClip;
-    public float flashSpeed = 5f;
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
-
-
-    Animator anim;
-    AudioSource playerAudio;
-    PlayerMovement playerMovement;
-    //PlayerShooting playerShooting;
-    bool isDead;
-    bool damaged;
-
-
-    void Awake ()
+    public class PlayerHealth : MonoBehaviour
     {
-        anim = GetComponent <Animator> ();
-        playerAudio = GetComponent <AudioSource> ();
-        playerMovement = GetComponent <PlayerMovement> ();
-        //playerShooting = GetComponentInChildren <PlayerShooting> ();
-        currentHealth = startingHealth;
-    }
+        [SerializeField] int startingHealth = 100;                              // Amount of health at start
+        public int currentHealth;                                               // Current HP
+        [SerializeField] Slider healthSlider;                                   // UI health bar
+        [SerializeField] Image damageImage;                                     // Reference to the image
+        [SerializeField] AudioClip deathClip;                                   // Sound to play
+        [SerializeField] float flashSpeed = 5f;                                 // The speed the damageImage will fade at.
+        [SerializeField] Color flashColour = new Color(1f, 0f, 0f, 0.1f);       // The colour the damageImage is set to, to flash.
+        float timeFromLastHit = Time.time;                                      // Captures the time
+        [SerializeField] int regenTime = 5;    	    				        	// how long it takes to start healing
+        [SerializeField] int regenPts = 1;    	    				        	// how many health points we get per "tick"
+        [SerializeField] Text healthText;                                       // a reference to the text
 
 
-    void Update ()
-    {
-        if(damaged)
+        Animator anim;                                                          // Reference to the Animator component.
+        AudioSource playerAudio;                                                // Reference to the AudioSource component.
+        PlayerMovement playerMovement;                                          // Reference to the player's movement.
+        PlayerShooting playerShooting;                                          // Reference to the PlayerShooting script.
+        bool isDead;                                                            // Whether the player is dead.
+        bool damaged;                                                           // True when the player gets damaged.
+
+
+        void Awake ()
         {
-            damageImage.color = flashColour;
+            // References
+            anim = GetComponent <Animator> ();
+            playerAudio = GetComponent <AudioSource> ();
+            playerMovement = GetComponent <PlayerMovement> ();
+            playerShooting = GetComponentInChildren <PlayerShooting> ();
+
+            currentHealth = startingHealth;
         }
-        else
+
+
+        void Update ()
         {
-            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-        }
-        damaged = false;
-    }
+			// If the player has just been damaged...
+			if (damaged) {
+				damageImage.color = flashColour;
+			}
+            else {
+				damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+			}
 
+			// reset flag
+			damaged = false;
+       
+			//let's be nice and have the player regenerate health after a while.
+			if ((Time.time - timeFromLastHit) >= regenTime && !isDead) { // check if dead and time's passed enough
+				if (currentHealth < startingHealth) {
+					currentHealth = currentHealth + regenPts;
+					healthSlider.value = currentHealth;
+                    healthText.text = currentHealth.ToString();
+                }
+			}
+		}
 
-    public void TakeDamage (int amount)
-    {
-        damaged = true;
-
-        currentHealth -= amount;
-
-        healthSlider.value = currentHealth;
-
-        playerAudio.Play ();
-
-        if(currentHealth <= 0 && !isDead)
+        public void TakeDamage (int amount)
         {
-            Death ();
+            // set flag
+            damaged = true;
+
+            currentHealth -= amount;
+
+            // Set the health bar's value to the current health.
+            healthSlider.value = currentHealth;
+            healthText.text = currentHealth.ToString();
+
+            // Play the hit sound
+            playerAudio.Play ();
+
+			//Now set the time since last hit.
+			timeFromLastHit = Time.time;
+
+            // doublecheck
+            if(currentHealth <= 0 && !isDead)
+            {
+                // ... it should die.
+                Death ();
+            }
+        }
+
+
+        void Death ()
+        {
+            // Set the death flag so this function won't be called again.
+            isDead = true;
+
+            // Turn off any remaining shooting effects.
+            playerShooting.DisableEffects ();
+
+            // Tell the animator that the player is dead.
+            anim.SetTrigger ("Die");
+
+            // Set the audiosource to play the death clip and play it (this will stop the hurt sound from playing).
+            playerAudio.clip = deathClip;
+            playerAudio.Play ();
+
+            // Turn off the movement and shooting scripts.
+            playerMovement.enabled = false;
+            playerShooting.enabled = false;
+        }
+
+
+        public void RestartLevel ()
+        {
+            // Reload the level that is currently loaded.
+            Application.LoadLevel (Application.loadedLevel);
         }
     }
-
-
-    void Death ()
-    {
-        isDead = true;
-
-        //playerShooting.DisableEffects ();
-
-        anim.SetTrigger ("Die");
-
-        playerAudio.clip = deathClip;
-        playerAudio.Play ();
-
-        playerMovement.enabled = false;
-        //playerShooting.enabled = false;
-    }
-
-
-    public void RestartLevel ()
-    {
-        Application.LoadLevel (Application.loadedLevel);
-    }
-}
