6987842711333e6688354589526d4eea3626e221
Jacob Dubose <jdubose@utc4me.org>
2015-11-17 10:31:09 -0500

    Orb regen on its way
    hnnnnn

---
+++ ZT/Assets/OrbRandomizer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbRandomizer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        RandomizePositions();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void RandomizePositions()
+    {
+        GameObject[] orbs = GameObject.FindGameObjectsWithTag("Boosters");
+        foreach (GameObject target in orbs)
+        {
+            Debug.Log(target.name);
+            target.transform.position = new Vector3(Random.Range(-120, 120), 1.2f, Random.Range(-120, 120));
+        }
+    }
+
+    IEnumerator UpdateOrbs()
+    {
+        yield return new WaitForSeconds(60);
+        RandomizePositions();
+    }
+}
